45d3a6fbeb10414df63c893b50de0617e0539c75
eperosk <oskar.j.persson@gmail.com>
2017-04-20 13:40:28 +0200

    TEST

--- src/main/scala/com/bth/app/MyScalatraServlet.scala
+++ src/main/scala/com/bth/app/MyScalatraServlet.scala
@@ -182,7 +182,7 @@ class MyScalatraServlet extends ScalatraoauthclientStack {
     this.setCookie(jsonString)
     out.println(userInfo)
     out.println("=============")
-
+    val aaf = "asaf"
     redirect("/")
   }
 
